928ffb548f8a293dd6db376223aeb986227e91cc
Matthias Brück <hi@brueck.io>
2024-11-01 20:54:20 +0100

    Add user bill luxury sum

--- pumpwerk/food/admin.py
+++ pumpwerk/food/admin.py
@@ -97,7 +97,7 @@ def calculate_account(modeladmin, request, queryset):
 calculate_account.short_description = "Calculate selected Accounts"
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ('title', 'inventory_date', 'terra_food_others_sum', 'terra_food_others_fee_sum', 'terra_brutto_all_sum', 'food_expenses_pumpwerk_sum', 'terra_food_pumpwerk_fee', 'luxury_paid_diff', 'terra_deposit_sum', 'attendance_day_sum', 'previous_terra_daily_rate', 'corrected_terra_daily_rate')
+    list_display = ('title', 'inventory_date', 'terra_food_others_sum', 'terra_food_others_fee_sum', 'terra_brutto_all_sum', 'food_expenses_pumpwerk_sum', 'terra_food_pumpwerk_fee', 'user_bill_luxury_sum', 'luxury_paid_diff', 'terra_deposit_sum', 'attendance_day_sum', 'previous_terra_daily_rate', 'corrected_terra_daily_rate')
     readonly_fields = ['additional_inventory_food', 'additional_inventory_luxury', 'terra_luxury_sum', 'luxury_consumed', 'luxury_paid_diff', 'terra_brutto_all_sum', 'terra_food_others_sum', 'terra_food_pumpwerk_fee', 'terra_brutto_others_sum', 'terra_deposit_sum', 'terra_food_pumpwerk_sum', 'food_expenses_pumpwerk_sum', 'attendance_day_sum', 'corrected_terra_daily_rate']
     actions = [calculate_account]
 
